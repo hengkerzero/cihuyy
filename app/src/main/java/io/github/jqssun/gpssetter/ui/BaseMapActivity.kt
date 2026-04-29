e47c5b39180787d50d65962422a0f287fd49d961
SENDY FERRY SETYAWAN <hengkerzero@gmail.com>
2026-04-29 21:49:59 +0700

    Update BaseMapActivity.kt

--- app/src/main/java/io/github/jqssun/gpssetter/ui/BaseMapActivity.kt
+++ app/src/main/java/io/github/jqssun/gpssetter/ui/BaseMapActivity.kt
@@ -471,7 +471,7 @@ abstract class BaseMapActivity: AppCompatActivity() {
                     it.priority = NotificationCompat.PRIORITY_HIGH
                     it.addAction(
                         R.drawable.ic_stop,
-                        "Stop GPS",
+                        "Stop",
                         stopPendingIntent
                     )
                 }
